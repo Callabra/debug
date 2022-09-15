b52b4bd69169316812bd8b74747742bcf9452b36
Tyler Mothershead <tylermothershead@gmail.com>
2022-09-15 16:27:17 -0600

    altered File class to allow static method calls

--- lib/File.php
+++ lib/File.php
@@ -8,32 +8,32 @@ use Monolog\Handler\StreamHandler;
 class File {
 
 
-	public function error(string $message, array|object $context, $filename='ERRORS')
+	public static function error(string $message, array $context, $filename='ERRORS')
 	{
 
 		#$value = self::format($value);
 		$Logger = new \Monolog\Logger('log');
-		$Logger->pushHandler(new \Monolog\Handler\StreamHandler('/var/www/logs/' . $filename . '.log', Level::Error));
+		$Logger->pushHandler(new \Monolog\Handler\StreamHandler('/var/www/html/logs/' . $filename . '.log', Level::Error));
 		$Logger->error("",array($message => $context));
 
 	}
 
-	public static function warn(string $message,$context,$filename='WARNINGS')
+	public static function warn(string $message, array $context, $filename='WARNINGS')
 	{
 
 		#$value = self::format($value);
 		$Logger = new \Monolog\Logger('log');
-		$Logger->pushHandler(new \Monolog\Handler\StreamHandler('/var/www/logs/' . $filename . '.log', Level::Warning));
+		$Logger->pushHandler(new \Monolog\Handler\StreamHandler('/var/www/html/logs/' . $filename . '.log', Level::Warning));
 		$Logger->warning("",array($message => $context));
 
 	}
 
 	
-	public static function info(string $message,$context,$filename='INFO')
+	public static function info(string $message, array $context, $filename='INFO')
 	{
 		#$value = self::format($value);
 		$Logger = new \Monolog\Logger('log');
-		$Logger->pushHandler(new \Monolog\Handler\StreamHandler('/var/www/logs/' . $filename . '.log', Level::Info));
+		$Logger->pushHandler(new \Monolog\Handler\StreamHandler('/var/www/html/logs/' . $filename . '.log', Level::Info));
 		$Logger->info("",array($message => $context));
 	}
 
